788b0fd75870d95ca0e22a4d7e09cf9157c2ea01
Nikoloz Parkosadze <nikaparkosadze4@gmail.com>
2024-04-25 20:48:36 +0400

    Unit 1 hw done

---
+++ Unit1.js
@@ -0,0 +1,69 @@
+// Task 1
+
+console.log('Hello World');
+
+// Task 2
+
+let num = 10;
+console.log(num);
+
+// Task 3
+
+const sum = (a, b) => {
+    console.log(a + b);
+};
+
+sum(10, 20);
+
+// Task 4
+
+const subtract = (a, b) => {
+    console.log(a - b);
+};
+
+subtract(10, 5);
+
+// Task 5
+
+const multiply = (a, b) => {
+    console.log(a * b);
+};
+
+multiply(2, 2);
+
+// Task 6
+
+const divide = (a, b) => {
+    console.log(a / b);
+};
+
+divide(10, 2);
+
+// Task 7
+
+const remainder = (a, b) => {
+    console.log(a % b);
+};
+
+remainder(10, 3);
+
+// Task 8
+
+let quote =
+    'The world as we have created it is a process of our thinking. It cannot be changed without changing our thinking.';
+
+console.log(quote.length);
+
+// Task 9
+
+let string1 = 'Hello';
+let string2 = 'World';
+console.log(string1 + ' ' + string2);
+
+// Task 10
+
+console.log(quote.toUpperCase());
+
+// Task 11
+
+console.log(quote.toLowerCase());
